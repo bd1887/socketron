9a181e96ae2330b92b3b3cbc4c46b4d636fe4ba3
Brandon D <bd1887@gmail.com>
2019-08-10 11:46:09 +0100

    socket debugging

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,27 +11,26 @@ from chat.twitch_irc import TwitchIrc
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
-        self.accept()
-        # # TODO: Find a more sensible authentication strategy using twitch 'sub' id perhaps
-        # decoded = verify_and_decode_jwt(self.scope['cookies']['token'])
-        # if decoded['preferred_username'] == self.scope['url_route']['kwargs']['room_name']:
-        #     # Get user
-        #     self.user_id = decoded['sub']
-        #     self.twitch_user = Twitch_User.objects.get(pk=self.user_id)
-        #     self.chat_responses = ChatResponse.objects.filter(twitch_user=self.twitch_user)
+        # TODO: Find a more sensible authentication strategy using twitch 'sub' id perhaps
+        decoded = verify_and_decode_jwt(self.scope['cookies']['token'])
+        if decoded['preferred_username'] == self.scope['url_route']['kwargs']['room_name']:
+            # Get user
+            self.user_id = decoded['sub']
+            self.twitch_user = Twitch_User.objects.get(pk=self.user_id)
+            self.chat_responses = ChatResponse.objects.filter(twitch_user=self.twitch_user)
 
-        #     # Get channel name from route
-        #     self.channel = self.scope['url_route']['kwargs']['room_name']
+            # Get channel name from route
+            self.channel = self.scope['url_route']['kwargs']['room_name']
 
-        #     # Instantiate chat bot
-        #     self.bot = Bot(self.chat_responses)
+            # Instantiate chat bot
+            self.bot = Bot(self.chat_responses)
 
-        #     # Start a Twitch Chat listener on user's channel
-        #     self.twitch_chat = TwitchIrc(self.channel)
-        #     self.twitch_chat.listen(lambda msg: self.message(msg))
+            # Start a Twitch Chat listener on user's channel
+            self.twitch_chat = TwitchIrc(self.channel)
+            self.twitch_chat.listen(lambda msg: self.message(msg))
 
-        #     # Accept connection
-        #     self.accept()
+            # Accept connection
+            self.accept()
 
 
     def disconnect(self, close_code):
