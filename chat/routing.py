dc2b14eed84ade2dc7e041523adbb40b55fb871f
Brandon D <bd1887@gmail.com>
2019-08-10 09:34:49 +0100

    socket debugging

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,16 @@
 # chat/routing.py
 from django.conf.urls import url
+from django.conf import settings
+
 
 from . import consumers
 
-websocket_urlpatterns = [
-    url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
-]
+if settings.RUNNING_DEVSERVER:
+    websocket_urlpatterns = [
+        url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    ]
+else:
+    websocket_urlpatterns = [
+        url(r'^socketron/ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    ]
+
